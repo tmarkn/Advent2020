b8f83ffa90a9260a2b271dffd8298242ef0abe58
Tmarkn <39633888+tmarkn@users.noreply.github.com>
2020-12-02 08:42:25 -0500

    Day1 cleanup

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -1,26 +1,28 @@
-## get input
+# get input
 with open('Day1/input.txt', 'r') as f:
     inp = [int(x) for x in f.readlines()]
 
-def twoNums(arr):
-    ## first number
+# using two numbers
+def twoNums(arr, num):
+    # first number
     for x in range(len(arr)):
-        ## second number
+        # second number
         for y in range(x+1, len(arr)):
-            ## check for correct numbers and break
-            if arr[x] + arr[y] == 2020:
+            # check for correct numbers and break
+            if arr[x] + arr[y] == num:
                 return arr[x], arr[y], arr[x] * arr[y]
 
-def threeNums(arr):
-    ## first number
+# using three numbers
+def threeNums(arr, num):
+    # first number
     for x in range(len(arr)):
-        ## second number
+        # second number
         for y in range(x+1, len(arr)):
-            ## third number
+            # third number
             for z in range(x+2, len(arr)):
-                ## check for correct numbers and break
-                if arr[x] + arr[y] + arr[z] == 2020:
+                # check for correct numbers and break
+                if arr[x] + arr[y] + arr[z] == num:
                     return arr[x], arr[y], arr[z], arr[x] * arr[y] * arr[z]
 
-print(twoNums(inp))
-print(threeNums(inp))
+print('The two numbers are {} and {}\n\tTheir product is {}'.format(*twoNums(inp, 2020)))
+print('The three numbers are {}, {}, and {}\n\tTheir product is {}'.format(*threeNums(inp, 2020)))
